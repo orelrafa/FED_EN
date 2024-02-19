d56949653317e023ec856b68e19885adb77a2caf
orelrafa <orelrafailov1998@gmail.com>
2024-02-19 17:20:15 +0200

    updated idb.js to pass Haim's test

--- idb.js
+++ idb.js
@@ -1,147 +1,84 @@
-//Start of Haim Test
-async function test() {
-  const db = await idb.openCalorisDB("caloriesdb", 1);
-  const result1 = await db.addCalories({
-    calorie: 200,
-    category: "LUNCH",
-    Name: "glass of milk",
+"use strict";
+
+const idb = {};
+
+idb.openCaloriesDB = async (dbName, version) => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName, version);
+
+    request.onerror = function (event) {
+      console.error("An error happened with indexedDB:");
+      console.error(event);
+      reject(event);
+    };
+
+    request.onupgradeneeded = function () {
+      const db = request.result;
+      const store = db.createObjectStore("calories", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      store.createIndex("categoryIndex", "category", { unique: false });
+    };
+
+    request.onsuccess = function () {
+      const db = request.result;
+      resolve(db);
+    };
   });
-  const result2 = await db.addCalories({
-    calorie: 300,
-    category: "LUNCH",
-    Description: "pizza slice",
+};
+
+idb.addCalories = async (db, calorieData) => {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction("calories", "readwrite");
+    const store = transaction.objectStore("calories");
+    const request = store.add(calorieData);
+
+    request.onerror = function (event) {
+      console.error("Error adding calories:");
+      console.error(event);
+      reject(event);
+    };
+
+    request.onsuccess = function () {
+      resolve(true);
+    };
+
+    transaction.oncomplete = function () {
+      db.close();
+    };
   });
-  if (db) {
-    console.log("creating db succeeded");
-  }
-  if (result1) {
-    console.log("adding 1st cost succeeded");
-  }
-  if (result2) {
-    console.log("adding 2nd cost succeeded");
-  }
-}
-test();
-
-//end of haim test beginning of car demo
-var demo = {};
-//different web browsers might have different implementations
-window.indexedDB =
-  window.indexedDB ||
-  window.mozIndexedDB ||
-  window.webkitIndexedDB ||
-  window.msIndexedDB;
-//checking whether the web browser supports the IndexedDB database
-//if it doesn't then showing a message saying so
-if (!window.indexedDB) {
-  console.log("The web browser doesn't support IndexedDB");
-} else {
-  console.log("The web browser supports IndexedDB");
-}
-
-demo.data = [
-  { id: "1232523", brand: "Toyota", year: 2012, model: "Corola" },
-  { id: "2343434", brand: "Mazda", year: 2008, model: "6" },
-  { id: "2234345", brand: "Fiat", year: 2014, model: "500 Large" },
-  { id: "2234333", brand: "Fiat", year: 2011, model: "Bravo" },
-];
-
-demo.db;
-demo.request = window.indexedDB.open("carsdb", 1);
-
-demo.request.onerror = function (event) {
-  console.log("error: ");
-};
-demo.request.onsuccess = function (event) {
-  demo.db = demo.request.result;
-  console.log("success: " + demo.db);
 };
 
-//fires when we create a new database or when we increase the version number
-demo.request.onupgradeneeded = function (event) {
-  demo.db = event.target.result;
-  var objectStore = demo.db.createObjectStore("cars", { keyPath: "id" });
-  objectStore.createIndex("foodCategory", ["category"], { unique: false });
-  /* adds all of the cars in the data array
-  for (let i in demo.data) {
-    objectStore.add(demo.data[i]);
-  }
-  */
-};
+async function testHaim() {
+  try {
+    const db = await idb.openCaloriesDB("caloriesdb", 1);
+    const result1 = await idb.addCalories(db, {
+      calorie: 200,
+      category: "LUNCH",
+      Name: "glass of milk",
+    });
 
-function readItem() {
-  var transaction = demo.db.transaction(["cars"]);
-  var objectStore = transaction.objectStore("cars");
-  var request = objectStore.get("12121212");
-  request.onerror = function (event) {
-    console.log("readItem(): cannot find the data item");
-  };
-  request.onsuccess = function (event) {
-    if (request.result) {
-      console.log(
-        "readItem(): " +
-          request.result.brand +
-          ", " +
-          request.result.id +
-          ", " +
-          request.result.model
-      );
-    } else {
-      console.log("readItem(): cannot find the item");
+    const result2 = await idb.addCalories(db, {
+      calorie: 300,
+      category: "LUNCH",
+      Description: "pizza slice",
+    });
+
+    if (db) {
+      console.log("Creating db succeeded");
     }
-  };
-}
 
-function readAllItems() {
-  var objectStore = demo.db.transaction("cars").objectStore("cars");
-  let request = objectStore.openCursor();
-  request.onerror = function (event) {};
-  request.onsuccess = function (event) {
-    var cursor = event.target.result;
-    if (cursor) {
-      console.log(
-        "readAllItems(): key=" +
-          cursor.key +
-          " brand=" +
-          cursor.value.brand +
-          " model=" +
-          cursor.value.model +
-          " id=" +
-          cursor.value.id
-      );
-      cursor.continue();
-    } else {
-      console.log("readAllItems(): no more entries!");
+    if (result1) {
+      console.log("Adding 1st entry succeeded");
     }
-  };
+
+    if (result2) {
+      console.log("Adding 2nd entry succeeded");
+    }
+  } catch (error) {
+    console.error("Test failed:", error);
+  }
 }
 
-function addItem() {
-  var request = demo.db
-    .transaction(["cars"], "readwrite")
-    .objectStore("cars")
-    .add({ id: "12121212", brand: "BMW", year: 2009, model: "318" });
-  request.onsuccess = function (event) {
-    console.log("addItem(): the new data item was added to your database.");
-  };
-  request.onerror = function (event) {
-    console.log(
-      "addItem(): problem with adding the new data item to the database "
-    );
-  };
-}
-
-function removeItem() {
-  var request = demo.db
-    .transaction(["cars"], "readwrite")
-    .objectStore("cars")
-    .delete("12121212");
-  request.onsuccess = function (event) {
-    console.log("removeItem(): the data item was removed from the database");
-  };
-  request.onerror = function (event) {
-    console.log(
-      "removeItem(): problem with removing a data item from the database"
-    );
-  };
-}
+testHaim();
