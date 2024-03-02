eced07c6779f31680461155f575a0049eb4ee00a
orelrafa <orelrafailov1998@gmail.com>
2024-03-02 21:54:15 +0200

    refactored code and exposed idb.js functions to the object calling openCalorisDB

--- idb.js
+++ idb.js
@@ -2,7 +2,7 @@
 
 const idb = {};
 
-idb.openCaloriesDB = async (dbName, version) => {
+idb.openCalorisDB = async (dbName, version) => {
   return new Promise((resolve, reject) => {
     const request = indexedDB.open(dbName, version);
 
@@ -28,156 +28,144 @@ idb.openCaloriesDB = async (dbName, version) => {
 
     request.onsuccess = function () {
       const db = request.result;
+      //Below we are exposing the functions to the db object
+      db.addCalories = async (calorieData) => {
+        return new Promise((resolve, reject) => {
+          const transaction = db.transaction("calories", "readwrite");
+          const store = transaction.objectStore("calories");
+          const request = store.add(calorieData);
+
+          request.onerror = function (event) {
+            console.error("Error adding calories:");
+            console.error(event);
+            reject(event);
+          };
+
+          request.onsuccess = function () {
+            resolve(request.result);
+          };
+
+          transaction.oncomplete = function () {
+            db.close();
+          };
+        });
+      };
+      db.getCaloriesByDate = async (startRange, endRange) => {
+        return new Promise((resolve, reject) => {
+          const transaction = db.transaction("calories", "readonly");
+          const store = transaction.objectStore("calories");
+          const index = store.index("calories_date");
+          const range = IDBKeyRange.bound(startRange, endRange, false, false);
+          let foodArrayRequest = index.getAll(range);
+
+          foodArrayRequest.onerror = function (event) {
+            console.error("Error getting calories:");
+            console.error(event);
+            reject(event);
+          };
+          foodArrayRequest.onsuccess = function () {
+            resolve(foodArrayRequest.result);
+          };
+          transaction.oncomplete = function () {
+            db.close();
+          };
+        });
+      };
+      db.deleteCalories = async (id) => {
+        //Delete food item from IndexedDB
+        return new Promise((resolve, reject) => {
+          const transaction = db.transaction("calories", "readwrite");
+          const store = transaction.objectStore("calories");
+          //"+" is needed because without it the id is a string and not int. IndexedDB quirk.
+          const request = store.delete(+id);
+          request.onerror = function (event) {
+            console.error(`Error deleting calories with id ${id}:`);
+            console.error(event);
+            reject(event);
+          };
+          request.onsuccess = function () {
+            resolve(true);
+          };
+          transaction.oncomplete = function () {
+            db.close();
+          };
+        });
+      };
+      db.updateFunction = function (existingCalories) {
+        //Getting the new food object properties
+        const newDescription = document.getElementById("descriptionEdit").value;
+        const newCalories = document.getElementById("caloriesEdit").value;
+        const newCategory = document.getElementById("categoryEdit").value;
+        //Updating the food object properties to the new ones
+        existingCalories.description = newDescription;
+        existingCalories.calories = newCalories;
+        existingCalories.selectedCategory = newCategory;
+        return existingCalories;
+      };
+      db.updateCalories = async (id) => {
+        return new Promise((resolve, reject) => {
+          const transaction = db.transaction("calories", "readwrite");
+          const store = transaction.objectStore("calories");
+          // Fetch the existing calorie object using the id.
+          const request = store.get(+id);
+          request.onsuccess = function (event) {
+            const existingCalories = event.target.result;
+            //console.log(`existingCalories: ${existingCalories}`);
+            // Modify the calorie using the update function
+            const updatedCalories = db.updateFunction(existingCalories);
+            // Put the updated calories back into the object store
+            const putRequest = store.put(updatedCalories);
+
+            putRequest.onsuccess = function () {
+              console.log("Calorie updated successfully");
+              resolve(true);
+            };
+            putRequest.onerror = function () {
+              console.error("Error updating calorie:", error);
+              reject(error);
+            };
+          };
+          request.onerror = function (error) {
+            console.error("Error fetching calorie:", error);
+            reject(error);
+          };
+          transaction.oncomplete = function () {
+            db.close();
+          };
+        });
+      };
       resolve(db);
     };
   });
 };
 
-idb.addCalories = async (db, calorieData) => {
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction("calories", "readwrite");
-    const store = transaction.objectStore("calories");
-    const request = store.add(calorieData);
-
-    request.onerror = function (event) {
-      console.error("Error adding calories:");
-      console.error(event);
-      reject(event);
-    };
-
-    request.onsuccess = function () {
-      resolve(request.result); //Changed from true to request.result
-    };
-
-    transaction.oncomplete = function () {
-      db.close();
-    };
-  });
-};
-
-idb.getCaloriesByDate = async (db, startRange, endRange) => {
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction("calories", "readonly");
-    const store = transaction.objectStore("calories");
-    const index = store.index("calories_date");
-    const range = IDBKeyRange.bound(startRange, endRange, false, false);
-    let foodArrayRequest = index.getAll(range);
-
-    foodArrayRequest.onerror = function (event) {
-      console.error("Error getting calories:");
-      console.error(event);
-      reject(event);
-    };
-    foodArrayRequest.onsuccess = function () {
-      resolve(foodArrayRequest.result);
-    };
-    transaction.oncomplete = function () {
-      db.close();
-    };
-  });
-};
-
-//Delete food item from IndexedDB
-idb.deleteCalories = async (db, id) => {
-  return new Promise((resolve, reject) => {
-    //console.log(db);
-    const transaction = db.transaction("calories", "readwrite");
-    const store = transaction.objectStore("calories");
-    //console.log(store);
-    //"+" is needed because without it the id is a string ant not int
-    const request = store.delete(+id);
-    //console.log("we in the delete from index function with id: ", id);
-
-    request.onerror = function (event) {
-      console.error(`Error deleting calories with id ${id}:`);
-      console.error(event);
-      reject(event);
-    };
-
-    request.onsuccess = function () {
-      resolve(true);
-    };
-
-    transaction.oncomplete = function () {
-      db.close();
-    };
-  });
-};
-
-idb.updateFunction = function (existingCalories) {
-  const newFoodName = document.getElementById("foodNameEdit").value;
-  const newCalories = document.getElementById("caloriesEdit").value;
-  const newCategory = document.getElementById("categoryEdit").value;
-  existingCalories.foodName = newFoodName;
-  existingCalories.calories = newCalories;
-  existingCalories.selectedCategory = newCategory;
-  return existingCalories;
-};
-
-idb.updateCalories = async (db, id) => {
-  return new Promise((resolve, reject) => {
-    const transaction = db.transaction("calories", "readwrite");
-    const store = transaction.objectStore("calories");
-    // Fetch the existing calorie using the id
-    const request = store.get(+id);
-
-    request.onsuccess = function (event) {
-      const existingCalories = event.target.result;
-      console.log(`existingCalories: ${existingCalories}`);
-      // Modify the calorie using the update function
-      const updatedCalories = idb.updateFunction(existingCalories);
-      // Put the updated calories back into the object store
-      const putRequest = store.put(updatedCalories);
-
-      putRequest.onsuccess = function () {
-        console.log("Calorie updated successfully");
-        resolve(true);
-      };
-      putRequest.onerror = function () {
-        console.error("Error updating calorie:", error);
-        reject(error);
-      };
-    };
-    request.onerror = function (error) {
-      console.error("Error fetching calorie:", error);
-      reject(error);
-    };
-    transaction.oncomplete = function () {
-      db.close();
-    };
-  });
-};
-
-async function test_getCalories() {
-  const db = await idb.openCaloriesDB("caloriesdb", 1);
-  const foodArray = await idb.getCaloriesByDate(db, "20240204", "20240204");
-  return foodArray;
-}
-
 //Q: Do the things it the test supposed to be shown in the UI?
 
 // async function testHaim() {
 //   try {
-//     const db = await idb.openCalorisDB("caloriesdb", 1);
-//     const result1 = await db.addCalories({
-//       calorie: 200,
-//       category: "LUNCH",
-//       description: "glass of milk",
-//     });
-//     const result2 = await db.addCalories({
-//       calorie: 300,
-//       category: "LUNCH",
-//       description: "pizza slice",
-//     });
-//     if (db) {
-//       console.log("creating db succeeded");
+//     async function test() {
+//       const db = await idb.openCalorisDB("caloriesdb", 1);
+//       const result1 = await db.addCalories({
+//         calorie: 200,
+//         category: "LUNCH",
+//         description: "glass of milk",
+//       });
+//       const result2 = await db.addCalories({
+//         calorie: 300,
+//         category: "LUNCH",
+//         description: "pizza slice",
+//       });
+//       if (db) {
+//         console.log("creating db succeeded");
+//       }
+//       if (result1) {
+//         console.log("adding 1st cost succeeded");
+//       }
+//       if (result2) {
+//         console.log("adding 2nd cost succeeded");
+//       }
 //     }
-//     if (result1) {
-//       console.log("adding 1st cost succeeded");
-//     }
-//     if (result2) {
-//       console.log("adding 2nd cost succeeded");
-//     }
+//     test();
 //   } catch (error) {
 //     console.error("Test failed:", error);
 //   }
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//DONE REFACTORING
 async function saveFood() {
   //Function to save the entered food information
 
@@ -6,75 +5,81 @@ async function saveFood() {
   const selectedDate = dateConvert(
     document.querySelector(".selected-date").textContent.split("/")
   );
-  const selectedCategory = document.getElementById("category").value;
-  const enteredFoodName = document.getElementById("foodName").value;
+  const enteredDescription = document.getElementById("description").value;
   const enteredCalories = document.getElementById("calories").value;
+  const selectedCategory = document.getElementById("category").value;
 
   // Check if the entered values in the fields are valid
-  if (!validateSaveFields(enteredFoodName, enteredCalories)) {
+  if (!validateSaveFields(enteredDescription, enteredCalories)) {
     return;
   }
 
-  // Push the new item to the database
+  // Push the new item to the database and save the indexedDB id to const id
   try {
-    const db = await idb.openCaloriesDB("caloriesdb", 1);
-    const id = await idb.addCalories(db, {
-      foodName: enteredFoodName,
+    const db = await idb.openCalorisDB("caloriesdb", 1);
+    const id = await db.addCalories({
+      description: enteredDescription,
       calories: enteredCalories,
       selectedCategory,
       selectedDate,
     });
 
     // Render the new item with the id attached to the html
-    renderFoodListItem(enteredFoodName, enteredCalories, selectedCategory, id);
+    renderFoodListItem(
+      enteredDescription,
+      enteredCalories,
+      selectedCategory,
+      id
+    );
   } catch (error) {
     console.error("Failed to add food item: ", error);
   }
-
   // Close the Modal on successful data entry
   closeFoodModal();
-
   // Clear the form fields for the next entry
   clearSaveFoodFields();
 }
-//DONE REFACTORING (MIGHT NEED TO COMBINE WITH EDIT)
+
 function clearSaveFoodFields() {
+  //Clears the form fields when closing the food addition modal
+  //also clears any errors that appeared
   const errFood = document.getElementById("errorMessageFood");
   const errCal = document.getElementById("errorMessageCal");
   errFood.textContent = "";
   errCal.textContent = "";
-  document.getElementById("foodName").value = "";
+  document.getElementById("description").value = "";
   document.getElementById("calories").value = "";
 }
 
-//DONE REFACTORING
-function setEditModalValues(foodName, calories, category) {
-  // Function that sets the values in the modal for editing
-  document.getElementById("foodNameEdit").value = foodName;
+function setEditModalValues(description, calories, category) {
+  // Function that sets the original values in the modal for editing
+  document.getElementById("descriptionEdit").value = description;
   document.getElementById("caloriesEdit").value = calories;
   document.getElementById("categoryEdit").value = category;
 }
 
-//DONE REFACTORING
 function validateSaveFields() {
+  //A form validation function for the food saving modal
   const errFood = document.getElementById("errorMessageFood");
   const errCal = document.getElementById("errorMessageCal");
-  const foodElement = document.getElementById("foodName");
+  const foodElement = document.getElementById("description");
   const caloriesElement = document.getElementById("calories");
 
   return validateFields(foodElement, caloriesElement, errFood, errCal);
 }
 
 function validateEditFoodFields() {
+  //A form validation function for the food saving modal
   const errFood = document.getElementById("errorMessageFoodEdit");
   const errCal = document.getElementById("errorMessageCalEdit");
-  const foodElement = document.getElementById("foodNameEdit");
+  const foodElement = document.getElementById("descriptionEdit");
   const caloriesElement = document.getElementById("caloriesEdit");
 
   return validateFields(foodElement, caloriesElement, errFood, errCal);
 }
 
 function validateFields(foodElement, caloriesElement, errFood, errCal) {
+  // a validation function for general use
   errFood.textContent = "";
   errCal.textContent = "";
 
@@ -100,9 +105,10 @@ function validateFields(foodElement, caloriesElement, errFood, errCal) {
   return true;
 }
 
-//Function to edit or delete food entries
 function editFood(clickedButton) {
-  const originalFoodName = clickedButton.querySelector("span").textContent;
+  //Function to edit or delete food entries
+  //Need to find a way to seperate into one for edit and one for delete
+  const originalDescription = clickedButton.querySelector("span").textContent;
   const originalCalories = clickedButton
     .querySelector(".badge")
     .textContent.split(" ")[0];
@@ -112,7 +118,7 @@ function editFood(clickedButton) {
   );
 
   // Set the values in the modal for editing
-  setEditModalValues(originalFoodName, originalCalories, originalCategory);
+  setEditModalValues(originalDescription, originalCalories, originalCategory);
 
   // Show the food edit modal
   foodEditModal.show();
@@ -125,7 +131,7 @@ function editFood(clickedButton) {
 
     // Update the food item in the list
     clickedButton.querySelector("span").textContent =
-      document.getElementById("foodNameEdit").value;
+      document.getElementById("descriptionEdit").value;
     clickedButton.querySelector(".badge").textContent = `${
       document.getElementById("caloriesEdit").value
     } Calories`;
@@ -148,8 +154,8 @@ function editFood(clickedButton) {
     console.log("updating calorie");
     const id = clickedButton.getAttribute("data-id");
     try {
-      const db = await idb.openCaloriesDB("caloriesdb", 1);
-      await idb.updateCalories(db, id);
+      const db = await idb.openCalorisDB("caloriesdb", 1);
+      await db.updateCalories(id);
     } catch (error) {
       console.error("Failed to update food item: ", error);
     }
@@ -166,8 +172,8 @@ function editFood(clickedButton) {
     //Delete from indexedDB
     const id = clickedButton.getAttribute("data-id");
     try {
-      const db = await idb.openCaloriesDB("caloriesdb", 1);
-      await idb.deleteCalories(db, id);
+      const db = await idb.openCalorisDB("caloriesdb", 1);
+      await db.deleteCalories(id);
     } catch (error) {
       console.error("Failed to delete food item: ", error);
     }
@@ -186,44 +192,39 @@ function editFood(clickedButton) {
 }
 
 function cleanEditErrorMessages() {
+  //Cleans any error messages that pop up while in the edit modal
   const errFood = document.getElementById("errorMessageFoodEdit");
   const errCal = document.getElementById("errorMessageCalEdit");
   errFood.textContent = "";
   errCal.textContent = "";
 }
-//DONE REFACTORING
 function deleteModalBackdrop() {
+  //A general function that deletes the semi-transparent background of bootstrap modals
   const backdrop = document.querySelector(".modal-backdrop");
   if (backdrop) {
     backdrop.remove();
   }
   document.body.classList.remove("modal-open");
 }
-//DONE REFACTORING - MIGHT NEED TO RENAME foodModal
 function closeFoodModal() {
+  // function that closes the food addition modal
   document.getElementById("foodModal").style.display = "none";
   document.querySelector(".modal-backdrop").remove();
 }
 
-//DONE REFACTORING
 async function renderFoodList(formattedDate) {
-  // Fetch array of food items for that specific day
-  const db = await idb.openCaloriesDB("caloriesdb", 1);
-  const foodArray = await idb.getCaloriesByDate(
-    db,
-    formattedDate,
-    formattedDate
-  );
+  // Fetch array of food items for the selected day
+  const db = await idb.openCalorisDB("caloriesdb", 1);
+  const foodArray = await db.getCaloriesByDate(formattedDate, formattedDate);
 
   // Render each food item in it's respective category
   foodArray.forEach((food) => {
-    const { foodName, calories, selectedCategory, id } = food; // added id
-    renderFoodListItem(foodName, calories, selectedCategory, id); // added id
+    const { description, calories, selectedCategory, id } = food; // added id
+    renderFoodListItem(description, calories, selectedCategory, id); // added id
   });
 }
 
-//DONE REFACTORING
-function renderFoodListItem(foodName, calories, selectedCategory, id) {
+function renderFoodListItem(description, calories, selectedCategory, id) {
   //create new list item with entered information
   const listItem = document.createElement("button");
   listItem.type = "button";
@@ -232,21 +233,22 @@ function renderFoodListItem(foodName, calories, selectedCategory, id) {
     "list-group-item-action",
     "rendered-group-item"
   );
-  listItem.innerHTML = `<span>${foodName}</span><span class="badge">${calories} Calories</span>`;
+  listItem.innerHTML = `<span>${description}</span><span class="badge">${calories} Calories</span>`;
   listItem.setAttribute("data-category", selectedCategory);
   listItem.setAttribute("data-id", id);
   //adds ability to edit food on click on item
   listItem.setAttribute("onClick", "editFood(this)");
 
-  //render the new list item on the corresponding category in the food list
+  //render the new list item in the corresponding category in the food list
   const foodList = document.getElementById("foodList");
   foodList
     .querySelector(`[data-category="${selectedCategory}"]`)
     .after(listItem);
 }
 
-//DONE REFACTORING
 function clearFoodList() {
+  //This function clears the rendered food items. Usually used when the food modal is closed
+  //so that new food items can be rendered on a freshly picked date
   //get the items that are rendered
   renderedFoodItems = document.querySelectorAll(".rendered-group-item");
   //remove the items that are rendered
@@ -255,8 +257,11 @@ function clearFoodList() {
   });
 }
 
-//DONE REFACTORING
 function dateConvert(selectedDate) {
+  //This function converts the date that appears at the top when clicking on a day in the
+  //calendar to an index key. We use the index key to render food items on a picked day.
+  //The function turns the DD/MM/YYYY into YYYYMMDD
+  //for example 29/5/1998 turns into 19980529
   const convertedDate = `${selectedDate[2]}${
     selectedDate[1] < 10
       ? selectedDate[1].toString().padStart(2, "0")
@@ -271,6 +276,10 @@ function dateConvert(selectedDate) {
 
 //DONE REFACTORING
 function toggleFoodCalendar() {
+  //The function is used to toggle between the food modal and the calendar
+  //When on a calendar and picking a day, the calendar vanishes and we are
+  //presented with a food modal. When clicking on the ("X") button to close the food modal,
+  //the food modal vanishes and the calendar appears
   const calendarContainer = document.querySelector(".calendar-container");
   const foodEditMenuContainer = document.querySelector(
     ".food-edit-menu-container"
@@ -279,14 +288,13 @@ function toggleFoodCalendar() {
   calendarContainer.classList.toggle("d-none");
   foodEditMenuContainer.classList.toggle("d-none");
   if (foodEditMenuContainer.classList.contains("d-none")) {
-    //clean the previous food list so that a fresh one can be rendered
+    //clean the previous food list when the modal closes so that a fresh one can be rendered later
     clearFoodList();
   }
 }
 
-//DONE REFACTORING
 function setSelectedCategoryInModal() {
-  // Set the selected category in the Modal after pressing "+". Example: I clicked "+" on Dinner so the category is set to "dinner".
+  // Set the selected category in the Modal after pressing "+". Example: I clicked "+" on Dinner so the category is set to "dinner". Runs only once in the project.
   document
     .getElementById("foodModal")
     .addEventListener("show.bs.modal", function (event) {
@@ -295,12 +303,11 @@ function setSelectedCategoryInModal() {
       document.getElementById("category").value = category;
     });
 }
-//-------------------------------------------------------------------------------------------------------------------------------
 
-//Calendar object
+//Calendar namespace
 
 const calendar = {};
-calendar.months = [
+calendar.MONTHS = [
   "January",
   "February",
   "March",
@@ -339,7 +346,7 @@ calendar.renderCalendar = function () {
 
   //Update header month year
   calendarHeaderMonthYear.textContent = `${
-    calendar.months[calendar.currentMonth]
+    calendar.MONTHS[calendar.currentMonth]
   } ${calendar.currentYear}`;
 
   //Clean body to allow dynamic population
